b5ea52e9e8a974806b82fd29a449ee33a7fa1ad0
alumno <alumno@utnfra.com.ar>
2019-10-08 11:48:08 -0300

    Clase 15 Practica Parcial

---
+++ Carbone.Lucas.2A/Clase15.Entidades(Practica Parcial)/Alumno.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clase15.EntidadesPracticaParcial
+{
+    public class Alumno
+    {
+        private string _apellido;
+        private int _legajo;
+        private string _nombre;
+        private float _nota;
+
+        #region Propiedades
+        public string Apellido
+        {
+            get { return this._apellido; }
+            set { this._apellido = value; }
+        }
+        public int Legajo
+        {
+            get { return this._legajo; }
+            set { this._legajo = value; }
+        }
+        public string Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+        public float Nota
+        {
+            get { return this._nota; }
+            set { this._nota = value; }
+        }
+
+        #endregion
+
+        #region Metodos
+        public Alumno (int legajo, string nombre, string apellido):this(nombre,apellido)
+        {
+            this.Legajo = legajo;
+        }
+        public Alumno(int legajo):this(0f)
+        {
+            this.Legajo = legajo;
+        }
+        public Alumno(string nombre,string apellido):this(0)
+        {
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+        }
+        public Alumno(float Nota):this()
+        {
+            this.Nota = Nota;
+        }
+        public Alumno()
+        {
+            this.Legajo = 0;
+            this.Nombre = "";
+            this.Apellido = "";
+            this.Nota = 0f;
+        }
+        private string Mostrar()
+        {
+            return "Legajo: "+this.Legajo+" Nombre: "+this.Nombre+" Apellido: "+this.Apellido+" Nota: "+this.Nota;
+        }
+        public static string Mostrar(Alumno alumno)
+        {
+            return alumno.Mostrar();
+        }
+        public static bool operator ==(Alumno a1, Alumno a2)
+        {
+            if(a1.Legajo == a2.Legajo)
+                return true;
+            return false;
+        }
+        public static bool operator !=(Alumno a1, Alumno a2)
+        {
+            return !(a1 == a2);
+        }
+
+        #endregion
+    }
+}
---
+++ Carbone.Lucas.2A/Clase15.Entidades(Practica Parcial)/Materia.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Clase15.EntidadesPracticaParcial
+{
+    public class Materia
+    {
+        private List<Alumno> _alumnos;
+        private EMateria _nombre;
+        private static Random _notaParaUnAlumno;
+
+        #region Propiedades
+        public List<Alumno> Alumnos
+        {
+            get { return this._alumnos; }
+            set { this._alumnos = value; }
+        }
+
+        public EMateria Nombre
+        {
+            get { return this._nombre; }
+            set { this._nombre = value; }
+        }
+        #endregion
+        #region Metodos
+        public void CalificarAlumnos()
+        {
+            foreach(Alumno a in this.Alumnos )
+            {
+                a.Nota = _notaParaUnAlumno.Next(1, 10);
+            }
+        }
+        public static explicit operator string(Materia materia)
+        {
+            return materia.Mostrar();
+        }
+        public static implicit operator float(Materia m)
+        {
+            int cantidadAlumnos = m.Alumnos.Count;
+            float notasTotales=0;
+            foreach(Alumno a in m.Alumnos)
+            {
+                notasTotales += a.Nota;
+            }
+            return notasTotales/cantidadAlumnos;
+        }
+        public static implicit operator Materia(EMateria nombre)
+        {
+            Materia e = new Materia();
+            e.Nombre = nombre;
+
+            return e;
+        }
+        private Materia()
+        {
+            this.Alumnos = new List<Alumno>();
+        }
+        static Materia() { _notaParaUnAlumno = new Random();  }
+        private Materia(EMateria nombre) { this.Nombre = nombre; }
+        private string Mostrar()
+        {
+            StringBuilder mostrar = new StringBuilder();
+            mostrar.AppendFormat("Materia: {0} \n", this.Nombre.ToString());
+            mostrar.AppendLine("************************************");
+            mostrar.AppendLine("****************ALUMNOS*************");
+            foreach(Alumno a in this.Alumnos)
+            {
+                mostrar.AppendFormat("{0}\n",Alumno.Mostrar(a));
+            }
+            return mostrar.ToString();
+        }
+        public static bool operator !=(Materia m, Alumno a)
+        {
+            return !(m == a);
+        }
+        public static Materia operator -(Materia m, Alumno a)
+        {
+            if(m==a)
+            {
+                m.Alumnos.Remove(a);
+            }
+            return m;
+        }
+        public static Materia operator +(Materia m, Alumno a)
+        {
+            if(m != a)
+            {
+                m.Alumnos.Add(a);
+            }
+            return m;
+        }
+        public static bool operator ==(Materia m, Alumno a)
+        {
+            foreach(Alumno alumno in m.Alumnos)
+            {
+                if(a == alumno)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        
+        #endregion
+    }
+}
