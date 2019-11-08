749e32d06376c08874477a9f1b8eaee74a73fb62
alumno <alumno@utnfra.com.ar>
2019-11-08 12:20:21 -0300

    Practica parcial 2

---
+++ Arquitecturas y SO/sistemasoperativos/hola.php
@@ -0,0 +1,9 @@
+<h1>hola php!!!</h1>
+<?php
+//$nombre = "alguien";
+//	  echo $nombre;
+	echo "<br> GET <br>";
+	var_dump($_GET);
+	echo "<br> POST <br>";
+	var_dump($_POST);
+?>
---
+++ Arquitecturas y SO/sistemasoperativos/index.php
@@ -0,0 +1,9 @@
+<?php
+//$nombre = "alguien";
+//	  echo $nombre;
+	echo "<br> GET <br>";
+	var_dump($_GET);
+	echo "<br> POST <br>";
+	var_dump($_POST);
+?>
+<h1>hola php!!!</h1>
